7c901c0c2cc464a6a3fe9d520c71330aecee197f
Abdullah Numan <anewman15@hotmail.com>
2021-03-15 21:38:13 +0600

    Define associations to User and Album models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,7 @@
 class Album < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :user_id, presence: true
+
+  belongs_to :user
+  has_man_attached :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_token
 
   validates :username, presence: true, uniqueness: true, length: { in: 5..15 }
+
+  has_many :albums
 end
