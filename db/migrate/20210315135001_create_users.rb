22275dd2f45f6834a469cee00b25a91d1bc0f6a0
Abdullah Numan <anewman15@hotmail.com>
2021-03-15 20:34:45 +0600

    Generate User model and run migration

---
+++ db/migrate/20210315135001_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :token, null: false
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :token, unique: true
+  end
+end
