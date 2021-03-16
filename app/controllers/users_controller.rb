fc149c369e20720e152a339be2e5783c3b6dfe7d
Abdullah Numan <anewman15@hotmail.com>
2021-03-16 15:17:52 +0600

    Define create action and sanitize params for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class API::V1::UsersController < ApplicationController
-  
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:username).permit(:username)
+  end
 end
